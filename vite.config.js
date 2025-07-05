7c6b4ef0f9c7fe7a2fb939426c1d16b72e0de03d
Eduard Guzman <eduardguzman@MacBook-Pro-Eduard.local>
2025-07-05 10:18:35 -0500

    fix: corrige configuración de Vite para Vercel

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
-});
+  root: '.', // asegura que la raíz es la carpeta actual
+  build: {
+    outDir: 'dist', // salida esperada por Vercel
+  },
+})
